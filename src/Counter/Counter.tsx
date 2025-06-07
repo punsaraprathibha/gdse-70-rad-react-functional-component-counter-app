ba6582aec2a3620ea9bff1f26cbbfb22803feaf7
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2025-06-07 12:03:40 +0530

    Added basic layout

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,42 +1,13 @@
 import './Counter.css';
-import {useEffect, useState} from "react";
 
 export function Counter(props: any) {
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        alert("componentDidMount: " +
-            "Component has been mounted! " +
-            "Props: " + props.data);
-        return () => {
-            alert("componentWillUnmount: " +
-                "component is being removed!")
-        }
-    }, []); // Run only Once (Empty dependency array
-
-    useEffect(() => {
-        alert("componentDidUpdate: " +
-            "Count has been updated!")
-    }, [count]);// Runs each time when count changes
-
-    const increment = () => {
-        setCount(
-            (prevCount) => prevCount + 1);
-    }
-
-    const decrement = () => {
-        setCount(
-            (prevCount) => prevCount - 1);
-    }
     return (
         <div className="counter">
             <h1>React Counter (Functional Components)</h1>
-            <h2>Count: {count}</h2>
+            <h2>Count: 0</h2>
             <div>
-                <button className="button"
-                         onClick={increment}>+</button>
-                <button className="button"
-                        onClick={decrement}>-</button>
+                <button className="button">+</button>
+                <button className="button">-</button>
             </div>
         </div>
     );
